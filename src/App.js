e7aef3c0b94bf95d7d46fd5707c9381fdfc772e0
김동욱 <igerapex@gmail.com>
2019-12-18 17:08:07 +0900

    Implement $set method deep updates

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
 module.exports = function update(prevState, changes) {
-  // type here
-
   for (const prop in changes) {
-    if (prop === '$set') {
-      return clone(changes[prop]);
-    }
     if (prop === '$push') {
-      return clone(prevState).concat(...changes[prop]);
+      return prevState.concat(...changes[prop]);
     }
     if (prop === '$unshift') {
-      return changes[prop].concat(...clone(prevState));
-    }
-    if (prop === '$merge') {
-      return { ...clone(prevState), ...changes[prop] };
+      return changes[prop].concat(...prevState);
     }
     if (prop === '$splice') {
       const [value] = changes[prop];
-      let nextState = clone(prevState);
+      let nextState = prevState.slice();
       nextState.splice(...value);
       return nextState;
     }
     if (prop === '$apply') {
       return changes[prop](prevState);
     }
+    if (prop === '$merge') {
+      return { ...prevState, ...changes[prop] };
+    }
+    if (prop === '$set') {
+      return changes[prop];
+    }
   }
 
-  function clone(obj) {
-    if (typeof obj !== 'object' || obj === null) return obj;
-    const copy = Array.isArray(obj) ? [] : {};
-    for (prop in obj) {
-      const value = obj[prop];
-      copy[prop] =
-        typeof value === 'object' && value !== null ? clone(value) : value;
+  const propList = [];
+  let value;
+  function findSetCommand(obj) {
+    for (const prop in obj) {
+      if (prop === '$set') {
+        return (value = obj[prop]);
+      } else {
+        propList.push(prop);
+        return findSetCommand(obj[prop]);
+      }
     }
-    return copy;
   }
+  findSetCommand(changes);
+
+  function makeNextState(changedValue, nestedPropList) {
+    for (let i = nestedPropList.length - 1; i >= 0; i--) {
+      if (i === nestedPropList.length - 1) {
+        obj[nestedPropList[i]] = changedValue;
+      } else {
+        let outer = {};
+        outer[nestedPropList[i]] = obj;
+        obj = outer;
+      }
+    }
+
+    return obj;
+  }
+  let nextState = makeNextState(value, propList);
+
+  return { ...prevState, ...nextState };
 };
